e8612c64865a90d55d1e8499d47f18c8c98a6c5b
Alef34 <julojenis@hotmail.com>
2026-07-29 13:28:59 +0200

    Fix CompanyManagementForm.Designer.cs - add InitializeComponent and Load event subscription

--- Forms/CompanyManagementForm.Designer.cs
+++ Forms/CompanyManagementForm.Designer.cs
@@ -12,5 +12,20 @@ namespace CompanyNotificationApp
             }
             base.Dispose(disposing);
         }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CompanyManagementForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Name = "CompanyManagementForm";
+            this.Text = "Správa firiem";
+            this.Load += new System.EventHandler(this.CompanyManagementForm_Load);
+            this.ResumeLayout(false);
+        }
     }
 }
