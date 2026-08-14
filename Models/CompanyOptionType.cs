7896f08e461fb0a6f8e881e6324bacbd400197d5
Julo Jenis <julojenis@hotmail.com>
2026-08-14 20:58:21 +0200

    vymazanie zbytocnych foldrov

--- Models/CompanyOptionType.cs
+++
@@ -1,9 +0,0 @@
-namespace CompanyNotificationApp.Models
-{
-    public enum CompanyOptionType
-    {
-        Employees = 1,    // Zamestnanci
-        VAT = 2,          // DPH
-        Slovakia = 3      // Slovensko
-    }
-}
